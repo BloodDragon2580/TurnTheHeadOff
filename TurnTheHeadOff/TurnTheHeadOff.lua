0f50b4a904312e9c10dccdea501ddd4dcab6aeb8
BloodDragon2580 <gewinner.thomas@gmail.com>
2022-06-11 15:34:46 +0200

    Update

--- TurnTheHeadOff/TurnTheHeadOff.lua
+++ TurnTheHeadOff/TurnTheHeadOff.lua
@@ -1,76 +1,17 @@
 local eventFrame = CreateFrame("FRAME");
 
-local function HACK_TalkingHeadFrame_PlayCurrent()
-	local frame = TalkingHeadFrame;
-	local model = frame.MainFrame.Model;
-	
-	if (frame.finishTimer) then
-		frame.finishTimer:Cancel();
-		frame.finishTimer = nil;
-	end
-
-	if (frame.voHandle) then
-		StopSound(frame.voHandle);
-		frame.voHandle = nil;
-	end
-	
-	local currentDisplayInfo = model:GetDisplayInfo();
-	local displayInfo, cameraID, vo, duration, lineNumber, numLines, name, text, isNewTalkingHead = C_TalkingHead.GetCurrentLineInfo();
-
-	if TurnTheHeadOffData[vo] then
-		return;
-	else
-		TurnTheHeadOffData[vo] = true;
-	end
-
-	local textFormatted = string.format(text);
-	if (displayInfo and displayInfo ~= 0) then
-		frame:Show();
-		if (currentDisplayInfo ~= displayInfo) then
-			model.uiCameraID = cameraID;
-			model:SetDisplayInfo(displayInfo);
-		else
-			if (model.uiCameraID ~= cameraID) then
-				model.uiCameraID = cameraID;
-				Model_ApplyUICamera(model, model.uiCameraID);
-			end
-
-			TalkingHeadFrame_SetupAnimations(model);
-		end
-		
-		if (isNewTalkingHead) then
-			TalkingHeadFrame_Reset(frame, textFormatted, name);
-			TalkingHeadFrame_FadeinFrames();
-		else
-			if (name ~= frame.NameFrame.Name:GetText()) then
-				frame.NameFrame.Fadeout:Play();
-				C_Timer.After(0.25, function()
-					frame.NameFrame.Name:SetText(name);
-				end);
-				C_Timer.After(0.5, function()
-					frame.NameFrame.Fadein:Play();
-				end);
-				
-				frame.MainFrame.TalkingHeadsInAnim:Play();
-			end
-
-			if ( textFormatted ~= frame.TextFrame.Text:GetText() ) then
-				frame.TextFrame.Fadeout:Play();
-				C_Timer.After(0.25, function()
-					frame.TextFrame.Text:SetText(textFormatted);
-				end);
-				C_Timer.After(0.5, function()
-					frame.TextFrame.Fadein:Play();
-				end);
-			end
-		end
-		
-		
-		local success, voHandle = PlaySound(vo, "Talking Head", true, true);
-		if (success) then
-			frame.voHandle = voHandle;
-		end
-	end
+local function TalkingHeadFrame()
+    if aura_env.config.showVoice then
+        local frame = TalkingHeadFrame;
+        if (frame.finishTimer) then
+            frame.finishTimer:Cancel();
+            frame.finishTimer = nil;
+        end
+        frame:Hide();
+    else    
+        TalkingHeadFrame_CloseImmediately()
+    end
+    return true
 end
 
 local function OnLoad()
@@ -78,7 +19,7 @@ local function OnLoad()
 		TurnTheHeadOffData = {};
 	end
 
-	TalkingHeadFrame_PlayCurrent = HACK_TalkingHeadFrame_PlayCurrent;
+	TalkingHeadFrame_PlayCurrent = TalkingHeadFrame;
 
 	eventFrame:SetScript("OnEvent", nil);
 	eventFrame:UnregisterEvent("ADDON_LOADED");
